6dc2caaac04572b16e05e15a2e8befce2a821cf6
Cuervolu <angel.cuervo187@gmail.com>
2024-08-23 23:08:12 -0400

    chore(backend): add mailing configuration

--- apps/backend/src/main/java/com/cortex/backend/Application.java
+++ apps/backend/src/main/java/com/cortex/backend/Application.java
@@ -1,13 +1,28 @@
 package com.cortex.backend;
 
+import com.cortex.backend.entities.Role;
+import com.cortex.backend.repositories.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
 	}
 
 }
