6d80bff796265a60ade94deb0ec1413912055aef
Cuervolu <angel.cuervo187@gmail.com>
2024-09-13 12:18:54 -0300

    chore(backend): add test controller

---
+++ apps/backend/src/main/java/com/cortex/backend/controllers/TestController.java
@@ -0,0 +1,22 @@
+package com.cortex.backend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+  @RequestMapping("/hello")
+  public String hello() {
+    return "Hello, World!";
+  }
+
+
+  @RequestMapping("/bye")
+  public String bye() {
+    return "Goodbye, World!";
+  }
+
+
+}
