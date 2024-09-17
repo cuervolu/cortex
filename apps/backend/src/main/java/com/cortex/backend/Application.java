5b181942eed77779850734623f54de17a7c35248
Cuervolu <angel.cuervo187@gmail.com>
2024-09-17 12:15:12 -0300

    refactor(backend): Enable caching
    - Enable caching in Application.java by adding @EnableCaching annotation
    - Modify CommandLineRunner bean in Application.java to use RoleService instead of RoleRepository
    - Update role initialization logic in CommandLineRunner bean to use RoleService methods instead of RoleRepository methods

--- apps/backend/src/main/java/com/cortex/backend/Application.java
+++ apps/backend/src/main/java/com/cortex/backend/Application.java
@@ -3,9 +3,11 @@ package com.cortex.backend;
 import com.cortex.backend.entities.user.Role;
 import com.cortex.backend.repositories.RoleRepository;
 import com.cortex.backend.services.CountryService;
+import com.cortex.backend.services.RoleService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -13,6 +15,7 @@ import java.util.List;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableCaching
 public class Application {
 
   public static void main(String[] args) {
@@ -20,13 +23,13 @@ public class Application {
   }
 
   @Bean
-  public CommandLineRunner runner(RoleRepository roleRepository, CountryService countryService) {
+  public CommandLineRunner runner(RoleService roleService, CountryService countryService) {
     return args -> {
       // Initialize roles
       List<String> roleNames = List.of("USER", "ADMIN", "MODERATOR", "MENTOR");
       for (String roleName : roleNames) {
-        if (roleRepository.findByName(roleName).isEmpty()) {
-          roleRepository.save(Role.builder().name(roleName).build());
+        if (roleService.findByName(roleName).isEmpty()) {
+          roleService.saveRole(Role.builder().name(roleName).build());
         }
       }
 
