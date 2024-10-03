c304afdeb110435fe0c98a6c7e535635f5cf843e
Cuervolu <angel.cuervo187@gmail.com>
2024-10-03 00:51:18 -0300

    refactor(tags): move TagRepository to tags/api package

--- apps/backend/education/src/main/java/com/cortex/backend/education/tags/api/TagRepository.java
+++ apps/backend/education/src/main/java/com/cortex/backend/education/tags/api/TagRepository.java
@@ -1,10 +1,12 @@
 package com.cortex.backend.education.internal;
 
 import com.cortex.backend.core.domain.Tag;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TagRepository extends CrudRepository<Tag, Long> {
+  Optional<Tag> findByName(String name);
 
 }
