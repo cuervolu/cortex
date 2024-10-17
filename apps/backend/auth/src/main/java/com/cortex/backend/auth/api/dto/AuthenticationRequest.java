2dda83eb71db8d70c698c9bb15182baec25a742b
Cuervolu <angel.cuervo187@gmail.com>
2024-10-17 19:28:14 -0300

    fix: InvalidDefinitionException in DTO

--- apps/backend/auth/src/main/java/com/cortex/backend/auth/api/dto/AuthenticationRequest.java
+++ apps/backend/auth/src/main/java/com/cortex/backend/auth/api/dto/AuthenticationRequest.java
@@ -3,12 +3,16 @@ package com.cortex.backend.auth.api.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class AuthenticationRequest {
 
