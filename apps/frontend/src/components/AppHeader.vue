91706d4a8b30e5e920d3bbfb0e47c977617f0647
Ignacio Carrasco <igna.carrasco@duocuc.cl>
2024-10-23 23:55:10 -0300

    feat: Update AppHeader and FooterSection components
    - Added NuxtLink to the logo in AppHeader
    - Updated route in AppHeader component
    - Imported defineProps in FooterSection component

--- apps/frontend/src/components/AppHeader.vue
+++ apps/frontend/src/components/AppHeader.vue
@@ -5,14 +5,16 @@ import {ChevronDown} from "lucide-vue-next";
 <template>
   <div class="w-screen px-10 py-5 flex-col justify-start items-center gap-2.5 inline-flex">
     <header class="self-stretch px-[39px] py-5 bg-[#fae7ef] rounded-[30px] justify-between items-center inline-flex">
-    <div class="w-[127px] h-[36.89px] justify-between items-center inline-flex">
+    <NuxtLink to="/">
+      <div class="w-[127px] h-[36.89px] justify-between items-center inline-flex">
       <img src="~/assets/img/Cortex%20Logo.svg" alt="Cortex Logo" class="w-8 h-[36.89px]">
       <span class="w-[90px] h-[25px] font-bold text-[#381653] leading-7 tracking-[4px]">CORTEX</span>
     </div>
+    </NuxtLink>
     <nav>
       <ul class="flex gap-6">
         <li class="px-2.5 py-1.5 justify-center items-center gap-2.5 flex">
-          <NuxtLink to="/explore-roadmaps" class="text-gray-700 hover:text-gray-900">Roadmaps</NuxtLink>
+          <NuxtLink to="/roadmaps" class="text-gray-700 hover:text-gray-900">Roadmaps</NuxtLink>
           <ChevronDown class="h-4 w-4 ml-1 text-gray-500"/>
         </li>
         <li class="px-2.5 py-1.5 justify-center items-center gap-2.5 flex">
@@ -29,7 +31,8 @@ import {ChevronDown} from "lucide-vue-next";
         </li>
       </ul>
     </nav>
-    <Button variant="default" class="bg-white text-center text-sm font-bold leading-7 rounded-[73px] px-8 py-3 h-[52px] text-[#381653] hover:bg-gray-100">Get started
+    <Button as-child variant="default" class="bg-white text-center text-sm font-bold leading-7 rounded-[73px] px-8 py-3 h-[52px] text-[#381653] hover:bg-gray-100">
+      <NuxtLink to="/auth/login">Comenzar</NuxtLink>
     </Button>
   </header>
   </div>
