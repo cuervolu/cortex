07e263da78467ef8e632f9f57fedd2ced544ad83
Ignacio Carrasco <igna.carrasco@duocuc.cl>
2024-10-23 23:53:54 -0300

    style: Add fill color to social icons for better visual consistency

--- apps/frontend/src/components/icons/SocialIcons.vue
+++ apps/frontend/src/components/icons/SocialIcons.vue
@@ -9,18 +9,18 @@ import InstagramIcon from "~/components/icons/InstagramIcon.vue";
   <div class="justify-center items-center gap-[11px] inline-flex">
     <a href="#" aria-label="Github"
       class="w-[41px] h-[41px] rounded-full border border-[#9cacaf] flex justify-center items-center transition-all hover:border-[#97c8d1] hover:bg-[#b3e1e9]">
-      <github-icon class="w-[22px] transition-transform hover:scale-110 hover:text-white" />
+      <github-icon class="fill-[#66667f] w-[22px] transition-transform hover:scale-110 hover:text-white" />
     </a>
     <!-- Twitter Icon -->
     <a href="#" aria-label="Twitter"
       class="w-[41px] h-[41px] rounded-full border border-[#9cacaf] flex justify-center items-center transition-all hover:border-[#97c8d1] hover:bg-[#b3e1e9]">
-      <twitter-icon class="w-[22px] transition-transform hover:scale-110 hover:text-white" />
+      <twitter-icon class="fill-[#66667f] w-[22px] transition-transform hover:scale-110 hover:text-white" />
     </a>
 
     <!-- Instagram Icon -->
     <a href="#" aria-label="Instagram"
       class="w-[41px] h-[41px] rounded-full border border-[#9cacaf] flex justify-center items-center transition-all hover:border-[#97c8d1] hover:bg-[#b3e1e9]">
-      <instagram-icon class="w-[22px] transition-transform hover:scale-110 hover:text-white" />
+      <instagram-icon class="fill-[#66667f] w-[22px] transition-transform hover:scale-110 hover:text-white" />
     </a>
   </div>
 </template>
