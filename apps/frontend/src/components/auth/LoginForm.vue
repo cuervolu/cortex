e035d6e61b9dd6ca508e2b5061420d27c287648a
Cuervolu <angel.cuervo187@gmail.com>
2024-10-29 20:23:08 -0300

    fix: Update minimum character limit for username in LoginForm

--- apps/frontend/src/components/auth/LoginForm.vue
+++ apps/frontend/src/components/auth/LoginForm.vue
@@ -20,7 +20,7 @@ const emit = defineEmits<{
 }>()
 
 const formSchema = toTypedSchema(z.object({
-  username: z.string().min(5, "Username must be at least 5 characters"),
+  username: z.string().min(3, "Username must be at least 5 characters"),
   password: z.string().min(8, 'Password must be at least 8 characters'),
 }))
 
