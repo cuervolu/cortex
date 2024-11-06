bb247b9dd38df65a30f8ae08bfc41a64ff79d13f
Cuervolu <angel.cuervo187@gmail.com>
2024-11-05 23:27:34 -0300

    feat: Add lombok annotations to ChangePasswordRequest (#88)
    Adds `@NoArgsConstructor` and `@AllArgsConstructor` annotations to the
    `ChangePasswordRequest` class to generate default and all-args constructors. This simplifies the class and makes it easier to create
    instances of the class.

--- apps/backend/user/src/main/java/com/cortex/backend/user/api/dto/ChangePasswordRequest.java
+++ apps/backend/user/src/main/java/com/cortex/backend/user/api/dto/ChangePasswordRequest.java
@@ -3,25 +3,27 @@ package com.cortex.backend.user.api.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChangePasswordRequest {
-  
-  @JsonProperty("current_password")
-  private String currentPassword;
-  
-  @NotEmpty(message = "New password is required")
-  @NotNull(message = "New password is required")
-  @JsonProperty("new_password")
-  private String newPassword;
-  
-  @NotEmpty(message = "Confirm password is required")
-  @NotNull(message = "Confirm password is required")
-  @JsonProperty("confirm_password")
-  private String confirmPassword;
+
+    @JsonProperty("current_password")
+    private String currentPassword;
+
+    @NotEmpty(message = "New password is required")
+    @NotNull(message = "New password is required")
+    @JsonProperty("new_password")
+    private String newPassword;
+
+    @NotEmpty(message = "Confirm password is required")
+    @NotNull(message = "Confirm password is required")
+    @JsonProperty("confirm_password")
+    private String confirmPassword;
 }
