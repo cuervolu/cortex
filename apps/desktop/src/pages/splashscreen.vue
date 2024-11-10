2fd183f811e55690e2e205556775d1660626501f
Ignacio Carrasco <102595964+Ignacio9172@users.noreply.github.com>
2024-11-09 23:01:10 -0300

    update-splashscreen-load-time-and-icons (#118)
    * ✨ feat: extend splash screen duration and improve UI
    
    Increase splash screen loading time from 3 to 8 seconds to enhance 
    user experience during app initialization. Update logo and 
    footer icons for better visual consistency and accessibility. 
    Refactor layout for improved responsiveness and aesthetics.
    
    * ✨ feat: update responsive design for auth layout
    
    Refactor the authentication layout to improve responsiveness. 
    Remove the unused scroll handling logic and add a media query 
    to hide text on smaller screens. This enhances the user 
    experience by ensuring the layout adapts better to various 
    screen sizes.

--- apps/desktop/src/pages/splashscreen.vue
+++ apps/desktop/src/pages/splashscreen.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import {invoke} from '@tauri-apps/api/core';
-import {open} from '@tauri-apps/plugin-shell';
-import {debug, error as logError} from "@tauri-apps/plugin-log";
-import {Github, Twitter} from 'lucide-vue-next'
-import {AppError} from "@cortex/shared/types";
-import DarkLogo from '~/assets/img/icon1_dark.svg'
-import CortexLogo from '~/assets/img/icon1.svg'
+import { invoke } from '@tauri-apps/api/core';
+import { open } from '@tauri-apps/plugin-shell';
+import { debug, error as logError } from "@tauri-apps/plugin-log";
+import TwitterIcon from '~/components/icons/TwitterIcon.vue';
+import GithubIcon from '~/components/icons/GithubIcon.vue';
+import { AppError } from "@cortex/shared/types";
+import CortexLogo from '~/assets/img/cortex_logo_dark_mode.svg'
 import SplashImg from '~/assets/img/splash_2.webp'
 import WaifuImg from '~/assets/img/splash_1.webp'
 
@@ -22,9 +22,9 @@ const initApp = async () => {
     await debug('Starting application initialization')
 
     // Simular tiempo de carga
-    await new Promise(resolve => setTimeout(resolve, 3000))
+    await new Promise(resolve => setTimeout(resolve, 8000))
 
-    // await invoke('close_splashscreen_show_main')
+    await invoke('close_splashscreen_show_main')
 
     await debug('Application initialization completed')
   } catch (error) {
@@ -39,59 +39,92 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="w-[580px] h-[400px] grid grid-cols-[245px_335px] overflow-hidden rounded-xl">
-    <!-- Left Column -->
-    <div class="bg-[#4A1D96] flex flex-col items-center justify-between p-6 text-white relative">
-      <!-- Logo -->
-      <div class="w-16 h-16 mb-4">
-        <img
-            :src="DarkLogo"
-            alt="Brain Logo"
-            width="64"
-            height="64"
-            class="w-full h-full"
-        >
+  <section class="w-screen h-screen flex overflow-hidden rounded-[22px] splash-background p-2.5">
+    <div class="flex flex-col gap-2.5">
+      <div class="w-12 h-12 bg-[#1d2127] rounded-lg justify-center items-center">
+        <img :src="CortexLogo" alt="Brain Logo" class="w-full h-full p-2.5" />
       </div>
-      <!-- Loading Text -->
-      <div class="text-center space-y-3 mb-8">
-        <h1 class="text-xl font-medium max-w-[200px]">
-          Espera un momento, estamos cargando tu aventura...
-        </h1>
-        <p class="text-lg opacity-80 animate-pulse uppercase">Conectando</p>
+      <div class="flex flex-col gap-3 py-7 justify-center items-center">
+        <img :src="CortexLogo" alt="Brain Logo" class="w-20 swing" />
+        <div class="flex flex-col gap-2.5 justify-center items-center text-[white]">
+          <span class="text-base text-center font-semibold tracking-wider">Espera un momento estamos cargando tu
+            aventura...</span>
+          <span class="text-[#c0c0c0] text-center text-sm font-normal">CONECTANDO</span>
+        </div>
       </div>
-      <!-- Footer -->
-      <div class="text-center space-y-2">
-        <p class="text-xs opacity-80">
-          Problemas de conexión? Háznoslo saber!
-        </p>
-        <div class="flex gap-3 justify-center">
-          <button
-              class="flex items-center gap-1.5 hover:opacity-80 transition-opacity text-sm"
-              @click="open('https://twitter.com/elonmusk')"
-          >
-            <Twitter class="w-4 h-4"/>
-            <span>Twitter</span>
-          </button>
-          <button
-              class="flex items-center gap-1.5 hover:opacity-80 transition-opacity text-sm"
-              @click="open('https://github.com/cuervolu/cortex')"
-          >
-            <Github class="w-4 h-4"/>
-            <span>Github</span>
-          </button>
+      <div class="flex flex-col justify-end items-center gap-2 h-full pb-3">
+        <span class="text-white text-center text-xs font-normal">Problemas de Conexión? Haznolo saber!</span>
+        <div class="flex gap-4 text-[#53B7BE]">
+          <div class="flex items-center gap-2 cursor-pointer"
+            @click="open('https://x.com/i/status/1854958268952854543')">
+            <div class="w-[18px] h-[18px] flex items-center p-[2px] rounded-[2px] bg-[#d9d9d9]">
+              <TwitterIcon :width="15" class="fill-[#1D1D1D]" />
+            </div>
+            <span class="text-sm">Twitter</span>
+          </div>
+          <div class="flex items-center gap-2 cursor-pointer" @click="open('https://github.com/cuervolu/cortex')">
+            <div class="w-[18px] h-[18px] flex items-center p-[2px] rounded-[2px] bg-[#d9d9d9]">
+              <GithubIcon :width="15" class="fill-[#1D1D1D]" />
+            </div>
+            <span class="text-sm">Github</span>
+          </div>
         </div>
       </div>
     </div>
-    <!-- Right Column - Illustration -->
-    <div class="bg-gradient-to-b from-[#E5F0FF] to-[#C7E1FF]">
-      <div class="w-full h-full relative">
-        <!-- Main Image -->
-        <img
-            :src="currentImage"
-            alt="Path Illustration"
-            class="w-full h-full object-contain"
-        >
-      </div>
+    <div class="min-w-[245px] relative overflow-hidden rounded-xl">
+      <img :src="currentImage" alt="" class="w-full h-full object-cover">
+      <div class="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent" />
     </div>
-  </div>
+
+  </section>
 </template>
+
+<style scoped>
+.splash-background {
+  background: linear-gradient(114deg, #381653 18.8%, #60246F 80.96%);
+}
+
+.swing {
+  -webkit-transform-origin: top center;
+  transform-origin: top center;
+  -webkit-animation-name: swing;
+  animation-name: swing;
+
+  /* Duración total de cada ciclo (1s de animación + 2s de pausa) */
+  -webkit-animation-duration: 3s;
+  animation-duration: 3s;
+
+  /* Repetir indefinidamente */
+  -webkit-animation-iteration-count: infinite;
+  animation-iteration-count: infinite;
+
+  /* Mantener la última posición de la animación antes de la siguiente ejecución */
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+}
+
+@keyframes swing {
+  0% {
+    transform: rotate3d(0, 0, 1, 0deg);
+  }
+  10% {
+    transform: rotate3d(0, 0, 1, 15deg);
+  }
+  20% {
+    transform: rotate3d(0, 0, 1, -10deg);
+  }
+  30% {
+    transform: rotate3d(0, 0, 1, 5deg);
+  }
+  40% {
+    transform: rotate3d(0, 0, 1, -5deg);
+  }
+  50% {
+    transform: rotate3d(0, 0, 1, 0deg);
+  }
+  100% {
+    transform: rotate3d(0, 0, 1, 0deg);
+  }
+}
+
+</style>
